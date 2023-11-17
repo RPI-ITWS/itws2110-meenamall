d9425f969edc2cfa62e5c641789152ae6b53afdf
Meena Mall <112650368+meenamall@users.noreply.github.com>
2023-11-17 13:52:09 -0500

    idk

--- quiz2/getjson.php
+++ quiz2/getjson.php
@@ -1,7 +1,6 @@
 <?php
 $mysqli = new mysqli("localhost", "lab6part3", "", "lab6");
 
-// Get the course parameter from the query string, default to 'Websys_course'
 $course = isset($_GET['course']) ? $_GET['course'] : 'Websys_course';
 
 $result = $mysqli->query("SELECT json FROM courses WHERE crn = 61971 AND course = '$course'");
